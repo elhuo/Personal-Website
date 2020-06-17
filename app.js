3b0fe17d67f0bc668fe8d911070536689e4d5242
elhuo <elliehuo24596@gmail.com>
2020-06-16 21:18:06 -0700

    Header and Footer done

--- app.js
+++ app.js
@@ -6,18 +6,27 @@ let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
 
 console.log('Here\'s a hidden message');
-// Automatic Slideshow - change image every 3 seconds
-var myIndex = 0;
-carousel();
 
-function carousel() {
+
+
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 3000);
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
